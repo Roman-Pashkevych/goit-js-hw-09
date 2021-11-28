ea4f6a5e6133a58881468b401d47fef0899d22bb
Roman-Pashkevych <pashkevych_roman@ukr.net>
2021-11-28 02:39:50 +0200

    HM 9 Finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnDataStart = document.querySelector('button[data-start]');
+const btnDataStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId;
+btnDataStart.addEventListener('click', onStart);
+btnDataStop.addEventListener('click', onStop);
+
+function onStart(event) {
+    event.currentTarget.setAttribute('disabled', 'true');
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStop(event) {
+    clearInterval(intervalId);
+    btnDataStart.removeAttribute('disabled');
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('[name="delay"]'),
+  inputStep: document.querySelector('[name="step"]'),
+  inputAmount:  document.querySelector('[name="amount"]'),
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  });
+};
+
+ 
+ function makePromises () {
+  let amountNumber = Number(refs.inputAmount.value);
+  let delayNumber = Number(refs.inputDelay.value);
+  let stepNumber = Number(refs.inputStep.value);
+   
+  for (let i = 1; i <= amountNumber; i++) {
+    createPromise(i, delayNumber)
+      .then(({ position, delay }) => {
+       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+      delayNumber += stepNumber;
   }
+  
 }
+ 
+const onFormsabmit = (evt) => {
+  evt.preventDefault();
+  makePromises()
+}
+
+refs.form.addEventListener('submit', onFormsabmit)
